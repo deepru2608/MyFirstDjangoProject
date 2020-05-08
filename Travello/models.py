fb0f91e82a90a588673d6c4a583ce38feb45da4d
Deepanshu Ruhela <deepanshuruhela4@outlook.com>
2020-05-09 00:01:53 +0530

    Add Some Stuff into it

--- Travello/models.py
+++ Travello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Destination:
+    Id: int
+    LocationName: str
+    Image: str
+    Description: str
+    Price: int
--- Travello/views.py
+++ Travello/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from Travello.models import Destination
+
+
 # Create your views here.
+
+
+def index(request):
+    dest1 = Destination()
+    dest1.LocationName = 'Delhi'
+    return render(request, 'index.html', {'dest1': dest1})
